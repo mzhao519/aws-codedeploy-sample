110348d628f6079ef62656f0039ae6482143629c
Simon Zhao <simon.zhao@codecanteen.com>
2017-01-19 00:57:48 +1100

    work for java7

--- src/action/TestServlet.java
+++ src/action/TestServlet.java
@@ -3,7 +3,6 @@ package action;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/")
+
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
