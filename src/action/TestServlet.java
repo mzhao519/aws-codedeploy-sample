8c89e87807a9ee8ec95e4cc0bd6103d1c2adab26
Simon Zhao <simon.zhao@codecanteen.com>
2017-01-20 12:20:25 +1100

    update pom

--- src/action/TestServlet.java
+++ src/action/TestServlet.java
@@ -3,6 +3,7 @@ package action;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +13,7 @@ import service.JdbcService;
 /**
  * Servlet implementation class TestServlet
  */
-
+@WebServlet("/")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
